01cdbc2886f5c2ac40fb5e250b060d5ffd6e95e3
misboelakademik <142501014+misboelakademik@users.noreply.github.com>
2023-08-17 18:28:17 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+// script.js (Example JS file, you can modify it accordingly)
+// This is an example file, you can add your own JavaScript code here if needed
+console.log("Hello, Modern Landing Page!");
